a2916881d065b2e9ff5282970461cbb1d6217691
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2024-04-14 19:21:00 +0200

    Fix : home page header

--- resources/views/article/list.blade.php
+++ resources/views/article/list.blade.php
@@ -3,20 +3,5 @@
 @section('title', 'Articles - Buzenco Financial Blog')
 
 @section('pagecontent')
-    <header class="w-full h-32 flex items-center justify-around px-2.5">
-        <div class="h-4/5 flex items-center gap-2">
-            <figure class="h-3/5 flex items-center justify-center overflow-hidden select-none">
-                <img class="h-full" src="https://i.ibb.co/HrbcTDk/buzenco-blog.png" alt="buzenco-blog" border="0" />
-            </figure>
-
-            <h2 class="blog-title-font text-4xl">Buzenco Financial Blog</h2>
-        </div>
-
-        <div class="h-4/5 flex items-center">
-            <a class="h-fit flex item-center px-6 text-lg" href="">Articles</a>
-            <a class="h-fit flex items-center px-6 text-lg" href="">Rédiger</a>
-        </div>
-    </header>
-
     <h1 class="TitleCheese">Liste des articles</h1>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -42,6 +42,21 @@
         </style>
     </head>
     <body>
+        <header class="w-full h-32 flex items-center justify-around px-2.5">
+            <div class="h-4/5 flex items-center gap-2">
+                <figure class="h-2/5 flex items-center justify-center overflow-hidden select-none">
+                    <img class="h-full" src="https://i.ibb.co/HrbcTDk/buzenco-blog.png" alt="buzenco-blog" border="0" />
+                </figure>
+
+                <h2 class="blog-title-font text-4xl">Buzenco Financial Blog</h2>
+            </div>
+
+            <div class="h-4/5 flex items-center">
+                <a class="h-fit flex item-center px-5 text-base" href="">Articles</a>
+                <a class="h-fit flex items-center px-5 text-base" href="">Rédiger</a>
+            </div>
+        </header>
+
         @yield('pagecontent')
     </body>
 </html>
